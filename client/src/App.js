4821b0aebdb7a6be2fed90402661765c7c0525db
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-20 21:02:58 -0400

    okta auth working

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from './containers/HomePage';
 import Navbar from './components/Navbar';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+const config = {
+  issuer: 'https://dev-845805.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oaf3yyjd25z2Coa90h7'
+}
 
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Navbar />
-          
-          <Switch>
-            <Route exact path='/' component={HomePage}/>
-          </Switch>
-          
-        </div>
+        <Security issuer={config.issuer}
+                  client_id={config.client_id}
+                  redirect_uri={config.redirect_uri}
+        >
+          <div>
+            <Navbar />
+            
+            <Switch>
+              <Route exact path='/' component={HomePage}/>
+              <Route path='/implicit/callback' component={ImplicitCallback} />
+            </Switch>
+            
+          </div>
+        </Security>
       </Router>
     );
   }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 import './Navbar.css';
 
 
-const Navbar = () => (
-    <nav className='navbar'>
-        <span className='homenav'><Link to='/'>Home</Link></span>
-        <span className='dynav'><Link to='/signup'> Sign Up</Link></span>
-        <span className='dynav'><Link to='/login'>Log In </Link></span>
-    </nav>
-)
+export default withAuth(class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { authenticated: null };
+        this.checkAuthentication = this.checkAuthentication.bind(this);
+        this.checkAuthentication();
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+    }
 
-export default Navbar;
+    async checkAuthentication() {
+        const authenticated = await this.props.auth.isAuthenticated();
+        if (authenticated !== this.state.authenticated) {
+            this.setState({ authenticated });
+        }
+    }
+
+    componentDidUpdate() {
+        this.checkAuthentication();
+    }
+
+    async login() {
+        this.props.auth.login('/');
+    }
+
+    async logout() {
+        this.props.auth.logout('/')
+    }
+
+    render() {
+        if (this.state.authenticated === null) return null;
+        return (
+            <nav className='navbar'>
+                <span className='homenav'><Link to='/'>Home</Link></span>
+                {this.state.authenticated ?
+                <span className='dynav'><button onClick={this.logout}>Log Out</button></span> :
+                <span className='dynav'><button onClick={this.login}>Login</button></span>}
+            </nav>
+        )
+    }
+})
