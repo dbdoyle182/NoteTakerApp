6d6e26cda52ec75ebcfdabd546bda96be1ca1a59
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-22 18:36:10 -0400

    not using okta anymore

--- server.js
+++ server.js
@@ -1,51 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const oktaJwtVerifier = require('@okta/jwt-verifier');
-var cors = require('cors')
+
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const oktaJwtVerifier = new OktaJwtVerifier({
-  issuer: 'https://dev-845805.oktapreview.com/oauth2/default',
-  assertClaims: {
-    aud: 'api://default',
-  }
-});
 
-
-function authenticationRequired(req, res, next) {
-  const authHeader = req.headers.authorization || '';
-  const match = authHeader.match(/Bearer (.+)/);
-
-  if(!match) {
-    return res.status(401).end();
-  }
-
-  const accessToken = match[1];
-
-  return oktaJwtVerifier.verifyAccessToken(accessToken)
-    .then(jwt => {
-      req.jwt = jwt;
-      next();
-    })
-    .catch(err => {
-      res.status(401).send(err.message);
-    });
-}
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
-
-app.get('/secure', authenticationRequired, (req,res) => {
-  res.json(req.jwt);
-});
-
-app.get('/api/messages', authenticationRequired, (req, res) => {
-  res.json([{
-    message: 'Hello, word!'
-  }]);
-});
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
